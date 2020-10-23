6db3628accb4cdfd588d2e4ace753ce1ec228ad7
Nijat Mursali <nijatmursali@gmail.com>
2020-10-23 22:05:38 +0200

    settings fix

--- admin/settings/settings.php
+++ admin/settings/settings.php
@@ -3,27 +3,22 @@
 require_once '../connection/connection.php';
 
 if (isset($_POST['submit'])) {
-    $submit = $cnn->prepare("UPDATE settings SET
-    
-    title=:title,
-    description=:description,
-    keyword=:keyword
-    
-    WHERE id=1
-    
-    ");
+    // set parameters and execute
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $keyword = $_POST['keyword'];
 
-    //echo $submit;
+    if ($cnn->connect_error) {
+        die("Connection failed: " . $cnn->connect_error);
+    }
 
-    $update = $submit->execute(array(
-        'title' => $_POST['title'],
-        'description' => $_POST['description'],
-        'keyword' => $_POST['keyword']
-    ));
+    $sql = "UPDATE settings SET title=" . "'" . $title . "'" . " WHERE id=1";
+    //echo $sql;
+    if ($cnn->query($sql) === TRUE) {
+        echo "Record updated successfully";
+    } else {
+        echo "Error updating record: " . $cnn->error;
+    }
 
-    // if ($update) {
-    //     echo "Successfully edited!";
-    // } else {
-    //     echo "Cannot edit!";
-    // }
+    $cnn->close();
 }
